2a0b751552361fdf37cd47cd06b5b374ceb69434
sourabhmadur <sourabhmadur@gmail.com>
2021-04-12 23:29:27 -0700

    fixing hover bug

--- src/Panel.js
+++ src/Panel.js
@@ -8,14 +8,14 @@ import equal from 'fast-deep-equal'
 class Panel extends Component{
     constructor(props){
     super(props);
-    this.state = {chart: null};
+    this.state = {chart: null, data : null};
     }
     
 
     componentDidMount(props){
         var ctx = document.getElementById(this.props.name).getContext('2d');
-        const data = require('./data/'+this.props.currentState +'.json');
-        this.setState({chart : new Chart(ctx, data)});
+        this.setState({data: require('./data/'+this.props.currentState +'.json')});
+        this.setState({chart : new Chart(ctx, this.state.data)});
     }
 
     componentDidUpdate(prevProps) {
@@ -23,8 +23,12 @@ class Panel extends Component{
             if(!equal(this.props.currentState, prevProps.currentState)) // Check if it's a new user, you can also use some unique property, like the ID  (this.props.user.id !== prevProps.user.id)
         {
             var ctx = document.getElementById(this.props.name).getContext('2d');
-            const data = require('./data/'+this.props.currentState +'.json');
-            this.setState({chart : new Chart(ctx, data)});
+            this.setState({data: require('./data/'+this.props.currentState +'.json')});
+            if(this.state.chart){
+                this.state.chart.destroy();
+            }
+
+            this.setState({chart : new Chart(ctx, this.state.data)});
         }
 
         } catch {
